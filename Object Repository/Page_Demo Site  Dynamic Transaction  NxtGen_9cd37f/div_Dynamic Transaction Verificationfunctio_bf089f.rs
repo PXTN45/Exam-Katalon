5e40d9c9b2124caa00cccabceef6da8520a4d41c
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-02-10 11:27:31 +0700

    first commit

---
+++ Object Repository/Page_Demo Site  Dynamic Transaction  NxtGen_9cd37f/div_Dynamic Transaction Verificationfunctio_bf089f.rs
@@ -0,0 +1,234 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Dynamic Transaction Verificationfunctio_bf089f</name>
+   <tag></tag>
+   <elementGuidId>4ee73d72-4bc7-45a8-bac8-ad77052b587e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#main</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='main']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>7d8acbc9-8404-4de4-bb0c-21efef333baa</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>main</value>
+      <webElementGuid>6dfcc221-0684-4b56-85c5-ee595aae0f27</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+	
+
+
+
+	
+			
+	
+
+    
+
+                    
+                                    		
+							
+					
+		
+					
+				
+				
+			Dynamic Transaction Verification
+
+
+function generateRandomNumber() {
+   let number = Math.floor((Math.random() * 100000))
+   document.write(&quot;Registration Form is Successfully Submitted. The Transaction ID : NXTGEN&quot;,number)  
+}
+
+generateRandomNumber();
+
+Registration Form is Successfully Submitted. The Transaction ID : NXTGEN21202		
+				
+					
+				
+					
+				
+							
+		                             
+        
+        
+        
+                    
+
+    
+
+           </value>
+      <webElementGuid>7bfda247-4ec9-4f94-9ea3-73d5c59a7689</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main&quot;)</value>
+      <webElementGuid>2cd5a6f8-268b-402b-8ec4-2c9c99d4e5a6</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='main']</value>
+      <webElementGuid>7d1a5952-4127-494f-9c23-94f66ca8fb42</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='wrapper']/div[2]</value>
+      <webElementGuid>1eba6dc9-e47a-4ec4-b2ea-573082943710</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Java'])[2]/following::div[2]</value>
+      <webElementGuid>cbe4b9a1-5532-407c-a344-89c9953f877a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Agile'])[2]/following::div[2]</value>
+      <webElementGuid>8f4b4bea-995c-4dc4-9804-30b0cf19dc9b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div[2]</value>
+      <webElementGuid>efb381f2-4747-48e5-b11c-e0bedd5b25a3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'main' and (text() = '
+
+	
+
+
+
+	
+			
+	
+
+    
+
+                    
+                                    		
+							
+					
+		
+					
+				
+				
+			Dynamic Transaction Verification
+
+
+function generateRandomNumber() {
+   let number = Math.floor((Math.random() * 100000))
+   document.write(&quot;Registration Form is Successfully Submitted. The Transaction ID : NXTGEN&quot;,number)  
+}
+
+generateRandomNumber();
+
+Registration Form is Successfully Submitted. The Transaction ID : NXTGEN21202		
+				
+					
+				
+					
+				
+							
+		                             
+        
+        
+        
+                    
+
+    
+
+           ' or . = '
+
+	
+
+
+
+	
+			
+	
+
+    
+
+                    
+                                    		
+							
+					
+		
+					
+				
+				
+			Dynamic Transaction Verification
+
+
+function generateRandomNumber() {
+   let number = Math.floor((Math.random() * 100000))
+   document.write(&quot;Registration Form is Successfully Submitted. The Transaction ID : NXTGEN&quot;,number)  
+}
+
+generateRandomNumber();
+
+Registration Form is Successfully Submitted. The Transaction ID : NXTGEN21202		
+				
+					
+				
+					
+				
+							
+		                             
+        
+        
+        
+                    
+
+    
+
+           ')]</value>
+      <webElementGuid>c673c0d3-0e53-415c-ae05-6641b498718b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Demo Site  Registration Form  NxtGen A_68ffcd/span_Convenient Time_select2-selection__arrow.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Convenient Time_select2-selection__arrow</name>
+   <tag></tag>
+   <elementGuidId>694bc15c-fb9b-4bd5-bfb2-183fa4bcf780</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>span.select2.select2-container.select2-container--default.select2-container--above.select2-container--open.select2-container--focus > span.selection > span.select2-selection.select2-selection--single > span.select2-selection__arrow</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//li[@id='item-vfb-16']/span/span/span/span/span[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>e1b1b513-0b59-48be-b95f-9c5c8c5341df</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>select2-selection__arrow</value>
+      <webElementGuid>8a066e3e-2dce-4b25-a766-dfec65eec9da</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>presentation</value>
+      <webElementGuid>c4c6a71f-81ff-4b0f-9654-175728d3e03b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;item-vfb-16&quot;)/span[@class=&quot;vfb-time&quot;]/span[@class=&quot;select2 select2-container select2-container--default select2-container--above select2-container--open select2-container--focus&quot;]/span[@class=&quot;selection&quot;]/span[@class=&quot;select2-selection select2-selection--single&quot;]/span[@class=&quot;select2-selection__arrow&quot;]</value>
+      <webElementGuid>3f740574-d2e1-460c-8145-08666baf11b4</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//li[@id='item-vfb-16']/span/span/span/span/span[2]</value>
+      <webElementGuid>eacc91bc-cd30-4ef2-ba5e-2402c3cc5210</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Convenient Time'])[1]/following::span[6]</value>
+      <webElementGuid>8decc992-dd76-4c79-a4d9-3cd872ae3f74</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Enter in MM/DD/YY Format'])[1]/following::span[6]</value>
+      <webElementGuid>7bd6e342-42a0-4ce3-986e-6e03eef88e95</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='HH'])[1]/preceding::span[2]</value>
+      <webElementGuid>de92b750-61e0-4b39-9f65-74418c05b463</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='MM'])[1]/preceding::span[8]</value>
+      <webElementGuid>c8b7d52e-8a93-4d2c-bb4d-e3a10c57857d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[7]/span/span/span/span/span[2]</value>
+      <webElementGuid>5daffc8f-15f3-4932-91ea-c8f735c36c25</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
